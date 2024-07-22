a114394469da92555246ee3f539fdaf3db304bb9
JiMo20 <Jmouhtasseb@gmail.com>
2024-07-22 13:06:43 +0400

    used async wait

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
+let list = document.getElementById("pok-list");
+
+let fetchPok_v2 = async () => {
+  let pokPromis = await fetch("https://pokeapi.co/api/v2/pokemon/").then(
+    (res) => {
+      return res.json();
+    }
+  );
+  return pokPromis;
+};
+
+window.onload = () => {
+  fetchPok_v2().then((data) => {
+    data.results.forEach((pok) => {
+      let li = document.createElement("li");
+      li.textContent = pok.name;
+      list.appendChild(li);
+      //add event listen to each li
+      li.addEventListener("click", () => {
+        console.log(pok);
+      });
+    });
+  });
+};
+
 // let list = document.getElementById("pokemon-list");
 
 // let fetchPok = () => {
 //   fetch("https://pokeapi.co/api/v2/pokemon/")
-//     .then(function (res) {
-//       return res.json();
-//     })
-//     .then(function (res) {
-//       console.log(res.results, "pok list");
-
-//       res.results.forEach(function (pokemon) {
-//         let listItem = document.createElement("li");
-//         listItem.textContent = pokemon.name;
-//         list.appendChild(listItem);
-//         listItem.addEventListener("click", () => {
+//     .then((res) => res.json())
+//     .then((res) => {
+//       res.results.forEach((pokemon) => {
+//         let li = document.createElement("li");
+//         li.textContent = pokemon.name;
+//         list.appendChild(li);
+//         li.addEventListener("click", () => {
 //           console.log(pokemon);
 //         });
 //       });
 //     })
-//     .catch(function (error) {
-//       console.error("Error fetching the Pokémon list:", error);
+//     .catch((error) => {
+//       console.error("Error fetching data:", error);
 //     });
 // };
+
 // window.onload = () => {
-//     fetchPok();
-//   };
-
-let list = document.getElementById("pokemon-list");
-
-let fetchPok = () => {
-  fetch("https://pokeapi.co/api/v2/pokemon/")
-    .then((res) => res.json())
-    .then((res) => {
-      res.results.forEach((pokemon) => {
-        let li = document.createElement("li");
-        li.textContent = pokemon.name;
-        list.appendChild(li);
-        li.addEventListener("click", () => {
-          console.log(pokemon);
-        });
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-};
-
-window.onload = () => {
-  fetchPok();
-};
+//   fetchPok();
+// };
