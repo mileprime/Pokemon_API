105d91b9fdfc7023fa2b4c2af4277644cbfcb0a3
JiMo20 <Jmouhtasseb@gmail.com>
2024-07-23 11:02:14 +0400

    added dadjokes

---
+++ dadjoke.js
@@ -0,0 +1,36 @@
+const url = "https://icanhazdadjoke.com";
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+// console.log(btn);
+// console.log(result);
+
+btn.addEventListener("click", () => {
+  fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+  result.textContent = "Loading...";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("error");
+    }
+    const data = await response.json();
+    console.log(data);
+    console.log(response);
+
+    result.textContent = data.joke;
+    console.log(data.joke);
+  } catch (error) {
+    console.log(error.message);
+    result.textContent = "There was an error...";
+  }
+};
+
+fetchDadJoke();
